7be159e0bd5b8f0b4e0af6fb8c9a53393f584f33
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-08-12 16:38:46 +0200

    Refactor of the assertion that a project folder exists

--- src/Solution.tests.ps1
+++ src/Solution.tests.ps1
@@ -11,37 +11,45 @@ Describe "Creating the .NET folder structure" {
 	$projectName = "GreenField"
 	$path = 'TestDrive:\'
 
-		Context "When the structure does not exist" {
+	Context "When the structure does not exist" {
 
-			New-DotNetProjectStructure -ProjectName $projectName -Path $path
+		New-DotNetProjectStructure -ProjectName $projectName -Path $path
+
+		function Assert-ThatProjectFolderExists{
+			param(
+				[Parameter(Mandatory = $true)]
+				[string] $ProjectFolder
+			)
+			Join-Path -Path "$path\$projectName" -ChildPath $ProjectFolder | Should Exist
+		}
 	
-			It "should create a root folder named after the project name"{
-				Join-Path -Path $path -ChildPath $projectName | Should Exist
-			}
+		It "should create a root folder named after the project name"{
+			Join-Path -Path $path -ChildPath $projectName | Should Exist
+		}
 
-			It "creates a docs subfolder" {
-				Join-Path -Path "$path\$projectName" -ChildPath "docs" | Should Exist
-			}
+		It "creates a docs subfolder" {
+			Assert-ThatProjectFolderExists "docs"
+		}
 
-			It "creates a src subfolder" {
-				Join-Path -Path "$path\$projectName" -ChildPath "src" | Should Exist
-			}
+		It "creates a src subfolder" {
+			Assert-ThatProjectFolderExists "src"
+		}
 
-			It "creates a samples subfolder" {			
-				Join-Path -Path "$path\$projectName" -ChildPath "samples" | Should Exist
-			}
+		It "creates a samples subfolder" {			
+			Assert-ThatProjectFolderExists "samples"
+		}
 
-			It "creates a build subfolder" {
-				Join-Path -Path "$path\$projectName" -ChildPath "build" | Should Exist
-			}
+		It "creates a build subfolder" {
+			Assert-ThatProjectFolderExists "build"
+		}
 
-			It "creates a tools subfolder" {
-				Join-Path -Path "$path\$projectName" -ChildPath "tools" | Should Exist
-			}
+		It "creates a tools subfolder" {
+			Assert-ThatProjectFolderExists "tools"
+		}
 
-			It "creates a packages subfolder" {
-				Join-Path -Path "$path\$projectName" -ChildPath "packages" | Should Exist
-			}
+		It "creates a packages subfolder" {
+			Assert-ThatProjectFolderExists "packages"
+		}
 	}
 }
 
