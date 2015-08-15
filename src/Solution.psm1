bb7d9b90a8533683e3f1847e9f78640680b5bde0
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-08-15 23:34:58 +0200

    Setup of NuGet files

--- src/Solution.psm1
+++ src/Solution.psm1
@@ -18,17 +18,27 @@ function New-DotNetProjectStructure {
 		New-Item -ItemType Directory -Path $Path -Name $ProjectName -Verbose
 
 		$projectPath = Join-Path -Path $Path -ChildPath $ProjectName
-												   
-		Push-Location $projectPath
-										   
+												   										   
 		New-Item -ItemType Directory -Path $projectPath -Name "docs" -Verbose
 		New-Item -ItemType Directory -Path $projectPath -Name "src" -Verbose
 		New-Item -ItemType Directory -Path $projectPath -Name "samples" -Verbose
 		New-Item -ItemType Directory -Path $projectPath -Name "build" -Verbose
-		New-Item -ItemType Directory -Path $projectPath -Name "tools" -Verbose
-		New-Item -ItemType Directory -Path $projectPath -Name "packages" -Verbose
+		New-Item -ItemType Directory -Path $projectPath -Name "tools" -Verbose | Resolve-Path -OutVariable toolsPath	
+		New-Item -ItemType Directory -Path $toolsPath -Name ".NuGet" -Verbose | Resolve-Path -OutVariable NuGetPath
+		
+		Invoke-WebRequest "https://nuget.org/nuget.exe" -OutFile "$NuGetPath\NuGet.exe" -Verbose
 
-		Pop-Location
+		$NuGetConfig = "<?xml version='1.0' encoding='utf-8'?>
+						<configuration>
+						  <solution>
+							<add key='disableSourceControlIntegration' value='true' />
+						  </solution>
+						  <packageSources>
+							<add key='nuget.org' value='https://www.nuget.org/api/v2/' />
+						  </packageSources>
+						</configuration>"
+		New-Item -ItemType File -Path "$NuGetPath\NuGet.Config" -Value $NuGetConfig -Verbose
+
 	}
 	End {
 
--- src/Solution.tests.ps1
+++ src/Solution.tests.ps1
@@ -4,51 +4,85 @@
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
 
-Import-Module .\src\Solution.psm1
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
 
-Describe "Creating the .NET folder structure" {
+Import-Module $dir\Solution.psm1
+
+Describe "Creating the .NET Path structure" {
 
 	$projectName = "GreenField"
 	$path = 'TestDrive:\'
+
+	$expectedProjectPath = Join-Path -Path $path -ChildPath $projectName
+	
+	$expectedDocumentationPath = Join-Path -Path $expectedProjectPath -ChildPath "docs"
+	$expectedSourcePath = Join-Path -Path $expectedProjectPath -ChildPath "src"
+	$expectedSamplesPath = Join-Path -Path $expectedProjectPath -ChildPath "samples"
+	$expectedBuildPath = Join-Path -Path $expectedProjectPath -ChildPath "build"
+	
+	$expectedToolsPath = Join-Path -Path $expectedProjectPath -ChildPath "tools"
+	$expectedNuGetPath = Join-Path -Path $expectedToolsPath -ChildPath ".NuGet"
+	$expectedNuGetExePath = Join-Path -Path $expectedNuGetPath -ChildPath  "NuGet.Exe"
+	$expectedNuGetConfigPath = Join-Path -Path $expectedNuGetPath -ChildPath  "NuGet.Config"
+	$absoluteNuGetConfigPath = Join-Path -Path $TestDrive -ChildPath (Split-Path -Path $expectedNuGetConfigPath -noQualifier)
 
 	Context "When the structure does not exist" {
 
 		New-DotNetProjectStructure -ProjectName $projectName -Path $path
 
-		function Assert-ThatProjectFolderExists{
-			param(
-				[Parameter(Mandatory = $true)]
-				[string] $ProjectFolder
-			)
-			Join-Path -Path "$path\$projectName" -ChildPath $ProjectFolder | Should Exist
-		}
-	
-		It "should create a root folder named after the project name"{
-			Join-Path -Path $path -ChildPath $projectName | Should Exist
+		It "should create a root Path named after the project name"{
+			$expectedProjectPath | Should Exist
 		}
 
-		It "creates a docs subfolder" {
-			Assert-ThatProjectFolderExists "docs"
+		It "creates a docs subPath" {
+			$expectedDocumentationPath | Should Exist
 		}
 
-		It "creates a src subfolder" {
-			Assert-ThatProjectFolderExists "src"
+		It "creates a src subPath" {
+			$expectedSourcePath | Should Exist
 		}
 
-		It "creates a samples subfolder" {			
-			Assert-ThatProjectFolderExists "samples"
+		It "creates a samples subPath" {			
+			$expectedSamplesPath | Should Exist
 		}
 
-		It "creates a build subfolder" {
-			Assert-ThatProjectFolderExists "build"
+		It "creates a build subPath" {
+			$expectedBuildPath | Should Exist
 		}
 
-		It "creates a tools subfolder" {
-			Assert-ThatProjectFolderExists "tools"
+		It "creates a tools subPath" {
+			$expectedToolsPath | Should Exist
 		}
 
-		It "creates a packages subfolder" {
-			Assert-ThatProjectFolderExists "packages"
+		It "creates a .NuGet subPath in de tools Path" {
+			$expectedNuGetPath | Should Exist
+		}
+
+		It "adds NuGet.exe to the .NuGet Path" {
+			$expectedNuGetExePath | Should Exist
+		}
+
+		It "should ensure that a NuGet.Config is present" {
+			$expectedNuGetConfigPath | Should Exist
+		}
+
+		It "should ensure that sourcecontrol integration for NuGet is disabled" {
+			$NuGetConfig = New-Object XML
+			$NuGetConfig.Load($absoluteNuGetConfigPath)
+
+			$isSourceControlEnabled = -Not (($NuGetConfig.configuration.solution.add | Where-Object { $_.GetAttribute("key") -match "disableSourceControlIntegration" }).value)
+
+			$isSourceControlEnabled | Should be $false
+		}
+		
+		It "should ensure that NuGet.org is added as a package source" {
+			$NuGetConfig = New-Object XML
+			$NuGetConfig.Load($absoluteNuGetConfigPath)
+
+			$NugetOrgPackageSource = ($NuGetConfig.configuration.packageSources.add | Where-Object { $_.GetAttribute("key") -match "nuget.org" }).value
+
+			$NugetOrgPackageSource | Should be "https://www.nuget.org/api/v2/"
 		}
 	}
 }
