e8caa31fd067961999abf3571732000fb5278416
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-09-14 11:35:21 +0200

    creating the folder structure and creating repository

--- src/Solution.psm1
+++ src/Solution.psm1
@@ -24,8 +24,6 @@ Function New-Solution
 	Param
 	(
 		[Parameter(Mandatory = $true)]
-		[string] $GitHubAccessToken,
-		[Parameter(Mandatory = $true)]
 		[string] $SolutionName,
 		[Parameter(Mandatory = $true)]
 		[System.IO.DirectoryInfo] $Path,
@@ -43,9 +41,9 @@ Function New-Solution
 			     
 		$useNuGetOrg = 
 		@{
-			Source = nuget.org;
-			ProviderName = NuGet;
-			Destination = $solutionFolderStructure.NuGetPackagesFolderPath;
+			Source = 'nuget.org';
+			ProviderName = 'NuGet';
+			Destination = $solutionFolderStructure.NuGetPackagesFolderDirectoryInfo;
 			Force = $true
 		}
 
@@ -54,10 +52,10 @@ Function New-Solution
 		Install-Package -Name GitVersion.CommandLine -MinimumVersion 3.1.2 -MaximumVersion 3.9.9 @useNuGetOrg
 		Install-Package -Name gitreleasemanager -MinimumVersion 0.3.0 -MaximumVersion 1.0.0 @useNuGetOrg
 		Install-Package -Name xunit.runner.console -MinimumVersion 2.0.0 -MaximumVersion 2.9.9 @useNuGetOrg
-		Install-Package -Name ILRepack -MinimumVersion 1.25.0 -MaximumVersion 1.9.9 @useNuGetOrg
+		Install-Package -Name ILRepack -MinimumVersion 2.0.5 -MaximumVersion 2.9.9 @useNuGetOrg
 		Install-Package -Name JetBrains.dotCover.CommandLineTools -MinimumVersion 3.2.2 -MaximumVersion 3.9.9 @useNuGetOrg
 
-		Invoke-WebRequest 'https://nuget.org/nuget.exe' -OutFile (Join-Path -Path $solutionFolderStructure.NuGetFolderPath -ChildPath 'nuget.exe') -Verbose
+		Invoke-WebRequest 'https://nuget.org/nuget.exe' -OutFile (Join-Path -Path $solutionFolderStructure.NuGetFolderDirectoryInfo.FullName -ChildPath 'nuget.exe') -Verbose
 
 		Initialize-GitFlow -Path $Path
 				
@@ -70,8 +68,6 @@ Function Join-Solution
 {
 	[CmdletBinding()]
 	Param (
-		[Parameter(Mandatory = $true, Position = 0)]
-		[string] $GitHubAccessToken,
 		[Parameter(Mandatory = $true)]
 		[string] $SolutionName,
 		[Parameter(Mandatory = $true)]
@@ -104,8 +100,6 @@ Function Rename-Solution
 	[CmdletBinding()]
 	Param 
 	(
-		[Parameter(Mandatory = $true, Position = 0)]
-		[string] $GitHubAccessToken,
 		[Parameter(Mandatory = $true)]
 		[string] $SolutionName
 	)
@@ -128,8 +122,6 @@ Function Add-License
 	[CmdletBinding()]
 	Param 
 	(
-		[Parameter(Mandatory = $true, Position = 0)]
-		[string] $GitHubAccessToken,
 		[Parameter(Mandatory = $true)]
 		[string] $SolutionName
 	)
@@ -161,28 +153,28 @@ Function New-SolutionFolderStructure
 	{	
 		$solutionFolderStructure = [SolutionFolderStructure]::New() 
 		@{	
-			SolutionFolderPathInfo = New-Item -ItemType Directory -Path $Path.FullName -Name $SolutionName -Verbose -Force | Resolve-Path -OutVariable SolutionFolderPath;
-			DocumentationFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $DocsFolderName -Verbose -Force;
-			SourcesFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $SourcesFolderName -Verbose -Force | Resolve-Path -OutVariable SourcesFolderPath;
-			TestsFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $TestsFolderName -Verbose -Force;
-			SamplesFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $SamplesFolderName -Verbose -Force;
-			BuildFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $BuildFolderName -Verbose -Force;
-			ArtifactsFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $ArtifactsFolderName -Verbose -Force;
-			NuGetFolderPathInfo = New-Item -ItemType Directory -Path $SourcesFolderPath -Name $NuGetFolderName -Verbose -Force;
-			NuGetPackagesFolderPathInfo = New-Item -ItemType Directory -Path $SolutionFolderPath -Name $NuGetPackagesFolderName -Verbose -Force;
+			SolutionFolderDirectoryInfo = New-Item -ItemType Directory -Path $Path.FullName -Name $SolutionName -Verbose -Force -OutVariable SolutionFolderPath;
+			DocumentationFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $DocsFolderName -Verbose -Force;
+			SourcesFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $SourcesFolderName -Verbose -Force -OutVariable SourcesFolderPath;
+			TestsFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $TestsFolderName -Verbose -Force;
+			SamplesFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $SamplesFolderName -Verbose -Force;
+			BuildFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $BuildFolderName -Verbose -Force;
+			ArtifactsFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $ArtifactsFolderName -Verbose -Force;
+			NuGetFolderDirectoryInfo = New-Item -ItemType Directory -Path $SourcesFolderPath.FullName -Name $NuGetFolderName -Verbose -Force;
+			NuGetPackagesFolderDirectoryInfo = New-Item -ItemType Directory -Path $SolutionFolderPath.FullName -Name $NuGetPackagesFolderName -Verbose -Force;
 		}
 	}
 }
 
 class SolutionFolderStructure
 {
-	[System.Management.Automation.PathInfo]$SolutionFolderPathInfo;
-	[System.Management.Automation.PathInfo]$DocumentationFolderPathInfo;
-	[System.Management.Automation.PathInfo]$SourcesFolderPathInfo;
-	[System.Management.Automation.PathInfo]$TestsFolderPathInfo;
-	[System.Management.Automation.PathInfo]$SamplesFolderPathInfo;
-	[System.Management.Automation.PathInfo]$BuildFolderPathInfo;
-	[System.Management.Automation.PathInfo]$ArtifactsFolderPathInfo;
-	[System.Management.Automation.PathInfo]$NuGetFolderPathInfo;
-	[System.Management.Automation.PathInfo]$NuGetPackagesFolderPathInfo;
+	[System.IO.DirectoryInfo]$SolutionFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$DocumentationFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$SourcesFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$TestsFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$SamplesFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$BuildFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$ArtifactsFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$NuGetFolderDirectoryInfo;
+	[System.IO.DirectoryInfo]$NuGetPackagesFolderDirectoryInfo;
 }
